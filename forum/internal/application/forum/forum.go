c2d7afb35b77edaa2278a82afaea4760afe4dc72
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-09-04 23:58:10 +0300

    create docker config

--- forum/internal/application/forum/forum.go
+++ forum/internal/application/forum/forum.go
@@ -12,19 +12,14 @@ import (
 	"gorm.io/gorm"
 )
 
-const (
-	forumLogsFile = "logs/forum.txt"
-	address       = "localhost:8080"
-)
-
 func Start() error {
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
 	)
 	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -36,16 +31,16 @@ func Start() error {
 		return fmt.Errorf("cannot up migrations: %w", err)
 	}
 
-	file, err := os.OpenFile(forumLogsFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
+	file, err := os.OpenFile(os.Getenv("LOGS_FILE"), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
 	if err != nil {
-		return fmt.Errorf("cannot open file %q: %w", forumLogsFile, err)
+		return fmt.Errorf("cannot open file %q: %w", os.Getenv("LOGS_FILE"), err)
 	}
 
 	logger := slog.New(slog.NewJSONHandler(file, &slog.HandlerOptions{}))
 	ctrl := controller.New(db, logger)
 
 	rout := router.New(ctrl)
-	rout.Run(address)
+	rout.Run(os.Getenv("API_ADDRESS"))
 
 	return nil
 }
