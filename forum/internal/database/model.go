33f4432c5db8fa028d7ac2302eab5ded8fdec36f
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-09-05 20:36:53 +0300

    change db models

--- forum/internal/database/model.go
+++ forum/internal/database/model.go
@@ -5,35 +5,35 @@ import (
 )
 
 type User struct {
-	ID           uint      `gorm:"primarykey; autoIncrement"`
-	Nickname     string    `gorm:"not null;"`
-	Email        string    `gorm:"not null; unique;"`
-	HashPassword string    `gorm:"not null;" json:"Password"`
-	RegisteredAt time.Time `gorm:"autoCreateTime"`
+	ID           uint      `gorm:"primarykey; autoIncrement" json:"id"`
+	Nickname     string    `gorm:"not null;" json:"nickname"`
+	Email        string    `gorm:"not null; unique;" json:"email"`
+	HashPassword string    `gorm:"not null;" json:"password"`
+	RegisteredAt time.Time `gorm:"autoCreateTime" json:"registered_at"`
 	Posts        []Post    `gorm:"foreignKey:AuthorID;" json:"-"`
 	Messages     []Message `gorm:"foreignKey:SenderID;" json:"-"`
 	Chats        []Chat    `gorm:"many2many:user_x_chat;" json:"-"`
 }
 
 type Post struct {
-	ID        uint   `gorm:"primarykey; autoIncrement"`
-	Content   string `gorm:"not null;"`
-	AuthorID  uint
-	CreatedAt time.Time
+	ID        uint      `gorm:"primarykey; autoIncrement" json:"id"`
+	Content   string    `gorm:"not null;" json:"content"`
+	AuthorID  uint      `json:"author_id"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Message struct {
-	ID       uint   `gorm:"primarykey; autoIncrement"`
-	Content  string `gorm:"not null;"`
-	SenderID uint
-	ChatID   uint
-	SendedAt time.Time `gorm:"autoCreateTime"`
+	ID       uint      `gorm:"primarykey; autoIncrement" json:"id"`
+	Content  string    `gorm:"not null;" json:"content"`
+	SenderID uint      `json:"sender_id"`
+	ChatID   uint      `json:"chat_id"`
+	SendedAt time.Time `gorm:"autoCreateTime" json:"sended_at"`
 }
 
 type Chat struct {
-	ID        uint   `gorm:"primarykey; autoIncrement"`
-	Name      string `gorm:"not null;"`
-	CreatedAt time.Time
+	ID        uint      `gorm:"primarykey; autoIncrement" json:"id"`
+	Name      string    `gorm:"not null;" json:"name"`
+	CreatedAt time.Time `json:"created_at"`
 	Members   []User    `gorm:"many2many:user_x_chat;" json:"-"`
 	Messages  []Message `gorm:"foreignKey:ChatID;" json:"-"`
 }
