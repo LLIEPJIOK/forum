af092c388e17ee62f7d9013a57de1e60772a64ed
MKvetko <m.kvetko@petrovich.tech>
2024-09-09 14:55:46 +0300

    Added post generation

--- de_airflow/dags/generation.py
+++ de_airflow/dags/generation.py
@@ -5,11 +5,10 @@ import requests
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
-
+import psycopg2
 
 
 API_KEY = Variable.get("API_KEY")
-POST_USER_URL = "http://forum-forum-1:8000/user"
 
 
 default_args = {
@@ -62,6 +61,50 @@ def batch_generate_users(batch: int, batch_iteration: int):
         for user in batched_users:
             requests.post(url="http://forum-forum-1:8000/user", data=json.dumps(user))            
         batched_users = []
+
+
+def create_postgress_connection():
+    conn = psycopg2.connect(
+    host="forum-db-1",
+    port="5430",
+    database="forumdb",
+    user="postgres",
+    password="some_password"
+)
+    return conn
+
+def format_post(id):
+    post = {}
+    text = get_post()
+    author_id = id
+    post['content'] = text
+    post['author_id'] = author_id
+    return post
+
+def get_all_users_id(connection):
+    cur = connection.cursor()
+    cur.execute("SELECT array_agg(id) FROM users;")
+    result = cur.fetchone()[0]
+    if len(result) == 0:
+        cur.close()
+        connection.close()
+        return
+    elif len(result) > 1 and len(result) < 5:
+        random_ids=random.sample(result,1)
+        post = format_post(random_ids[0])
+        requests.post("http://forum-forum-1:8000/post", data=json.dumps(post))
+    elif len(result) > 10:
+        random_ids=random.sample(result,8)
+        for i in random_ids:
+            post = format_post(i)
+            requests.post("http://forum-forum-1:8000/post", data=json.dumps(post))
+    cur.close()
+    connection.close()
+
+
+def generate_posts():
+    get_all_users_id(create_postgress_connection())
+    
         
 
 
@@ -75,7 +118,10 @@ with DAG('generate_users',
         python_callable=batch_generate_users,
         op_args=[5, 2]
     )
+    generate_posts=PythonOperator(
+        task_id="generate_posts",
+        python_callable=generate_posts
+    )
 
 
-
-generate_users
+generate_users >> generate_posts
